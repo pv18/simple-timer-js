46d434e5592c4ce87ea58593843a517409883788
pv18 <pv9998286620@yandex.ru>
2022-11-21 20:07:23 +0300

    added logic to the timer

--- index.js
+++ index.js
@@ -5,7 +5,25 @@ const timerEl = document.querySelector('span');
 // Напишите реализацию createTimerAnimator
 // который будет анимировать timerEl
 const createTimerAnimator = () => {
-    return (seconds) => {};
+    let interval;
+
+    return (seconds) => {
+        if (!seconds) timerEl.innerHTML = `hh:mm:ss`;
+
+        if (interval) {
+            clearInterval(interval);
+        }
+
+        interval = setInterval(function () {
+            if (seconds <= 0) {
+                clearInterval(interval);
+                return;
+            }
+            timerEl.innerHTML = getConvertTimeFormat(seconds);
+
+            ++seconds;
+        }, 1000);
+    };
 };
 
 const animateTimer = createTimerAnimator();
@@ -13,6 +31,7 @@ const animateTimer = createTimerAnimator();
 inputEl.addEventListener('input', () => {
     // Очистите input так, чтобы в значении
     // оставались только числа
+    inputEl.value = inputEl.value.replace(/[^0-9]/g, '');
 });
 
 buttonEl.addEventListener('click', () => {
@@ -24,19 +43,16 @@ buttonEl.addEventListener('click', () => {
 });
 
 
+function getConvertTimeFormat(value) {
+    let h = Math.floor(value / 3600);
+    let m = Math.floor(value % 3600 / 60);
+    let s = Math.floor(value % 3600 % 60);
 
+    let hDisplay = h < 10 ? '0' + h : h;
+    let mDisplay = m < 10 ? '0' + m : m;
+    let sDisplay = s < 10 ? '0' + s : s;
 
-
-
-
-
-
-
-
-
-
-
-
-
+    return `${hDisplay}:${mDisplay}:${sDisplay}`;
+}
 
 
